68ffb63bb08a37a01b43dd540b7e9fba23263ea7
Michael Chaberski <mchaberski@novetta.com>
2016-12-04 13:38:10 -0500

    day 4 puzzle 2

--- advent4_rooms.py
+++ advent4_rooms.py
@@ -6,6 +6,13 @@
 
 import sys, re
 
+_ALPHABET = 'abcdefghijklmnopqrstuvwxyz'
+assert len(_ALPHABET) == 26 and len(set(_ALPHABET)) == 26, "%s len=%d len(set)=%d" % (_ALPHABET, len(_ALPHABET), len(set(_ALPHABET)))
+
+def shift(x, n):
+    pos = _ALPHABET.index(x)
+    return _ALPHABET[(pos + n) % 26]
+
 def create_cmp(encrypted_name):
     def custom_cmp(a, b):
         na = len(filter(lambda x: x == a, encrypted_name))
@@ -32,6 +39,9 @@ class Room:
         s = ''.join(sorted(name_chars, cmp=c)[:5])
         return s 
 
+    def decrypt(self):
+        return ''.join([shift(x, self.sector) if x in _ALPHABET else x for x in self.name])
+
 def parse_room(code):
     patt = re.compile(r'^([-a-z]+)-(\d+)\[(\w+)\]$')
     m = patt.match(code)
@@ -51,6 +61,10 @@ def main(args):
     for room_code, expected in _TEST_CASES:
         room = parse_room(room_code)
         assert room.check_real() == expected, (room_code, expected, room.compute_checksum())
+    for ch, sh, expected in (('a', 1, 'b'), ('d', 26 * 50 + 3, 'g'), ('z', 0, 'z'), ('y', 2, 'a'), ('x', 26 * 2 + 5, 'c')):
+        assert shift(ch, sh) == expected, (ch, sh, expected, shift(ch, sh))
+    assert parse_room('qzmt-zixmtkozy-ivhz-343[zimth]').check_real()
+    assert parse_room('qzmt-zixmtkozy-ivhz-343[zimth]').decrypt() == 'very-encrypted-name'
     if len(args) == 0:
         input_text = sys.stdin.read()
     else:
@@ -59,7 +73,9 @@ def main(args):
     rooms = map(parse_room, room_codes)
     real_rooms = filter(lambda room: room.check_real(), rooms)
     sector_sum = sum([room.sector for room in real_rooms])
-    print "%d is sum of sector IDs of %d real rooms out of %d codes" % (sector_sum, len(real_rooms), len(rooms))
+    for room in real_rooms:
+        print room.decrypt(), room.name, room.sector
+    print >> sys.stderr, "%d is sum of sector IDs of %d real rooms out of %d codes" % (sector_sum, len(real_rooms), len(rooms))
     return 0
 
 if __name__ == '__main__':
