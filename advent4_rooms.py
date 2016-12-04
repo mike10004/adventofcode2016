f649d838e4a307656e803e3b9a0e6b467ba9c57c
Michael Chaberski <mchaberski@novetta.com>
2016-12-04 12:22:25 -0500

    day 4 puzzle 1 - started

---
+++ advent4_rooms.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  advent4_rooms.py
+#  
+
+
+def main(args):
+    return 1
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
