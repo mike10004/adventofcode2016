3198ff2b1e3688384344dfd2c1ba92727ccd6550
Michael Chaberski <mchaberski@novetta.com>
2016-12-19 14:28:04 -0500

    day 17 first hack

---
+++ advent17/src/main/java/aoc2016/day17/Game.java
@@ -0,0 +1,203 @@
+package aoc2016.day17;
+
+import com.google.common.collect.TreeTraverser;
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+import com.google.common.primitives.UnsignedBytes;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public class Game {
+
+    public final String passcode;
+    public final int gridWidth, gridHeight;
+    public final com.google.common.hash.HashFunction algorithm;
+    private final Charset charset = StandardCharsets.US_ASCII;
+
+    public Game(String passcode, int gridWidth, int gridHeight, HashFunction algorithm) {
+        this.passcode = checkNotNull(passcode);
+        this.algorithm = checkNotNull(algorithm);
+        this.gridHeight = gridHeight;
+        this.gridWidth = gridWidth;
+    }
+
+    public Game(String passcode) {
+        this(passcode, 4, 4, Hashing.md5());
+    }
+
+    public State play(int maxDepth) {
+        TreeTraverser<State> traverser = treeTraverser();
+        Iterable<State> bfs = traverser.breadthFirstTraversal(root());
+        for (State state : bfs) {
+            if (state.isWin()) {
+                return state;
+            }
+            if (state.level > maxDepth) {
+                return null;
+            }
+        }
+        return null;
+    }
+
+    State root() {
+        return new State(null, '\0');
+    }
+
+    State state(State previous, char direction) {
+        return new State(previous, direction);
+    }
+
+    private TreeTraverser<State> treeTraverser() {
+        return new TreeTraverser<State>() {
+            @Override
+            public Iterable<State> children(State root) {
+                return Arrays.asList(root.findNext());
+            }
+        };
+    }
+
+    @SuppressWarnings("WeakerAccess")
+    public class State {
+        public final int x, y;
+        public final State previous;
+        public final char direction;
+        public final char[] trail;
+        public final int level;
+        public State(State previous, char direction) {
+            this.previous = previous;
+            this.direction = direction;
+            if (previous != null) {
+                level = previous.level + 1;
+                x = previous.x + shiftX(direction);
+                y = previous.y + shiftY(direction);
+                trail = new char[previous.trail.length + 1];
+                System.arraycopy(previous.trail, 0, trail, 0, previous.trail.length);
+                trail[trail.length - 1] = direction;
+            } else {
+                level = 0;
+                x = 1;
+                y = 1;
+                trail = new char[0];
+            }
+        }
+
+        public String concatTrail() {
+            return new String(trail);
+        }
+
+        public boolean isWin() {
+            return x == gridWidth && y == gridWidth;
+        }
+
+        public State[] findNext() {
+            char[] open = findOpen();
+            State[] states = new State[open.length];
+            for (int i = 0; i < open.length; i++) {
+                states[i] = move(open[i]);
+            }
+            return states;
+        }
+
+        public State move(char direction) {
+            return new State(this, direction);
+        }
+
+        protected String computeHash() {
+            Hasher h = algorithm.newHasher(passcode.length() + trail.length);
+            h.putString(passcode, charset);
+            for (int i = 0; i < trail.length; i++) {
+                h.putByte(UnsignedBytes.checkedCast(trail[i]));
+            }
+            HashCode code = h.hash();
+            return code.toString();
+        }
+
+        public char[] findOpen() {
+            String code = computeHash();
+            String codeStr = code.substring(0, legend.length);
+            char[] buffer = new char[legend.length];
+            int nValid = 0;
+            for (int i = 0; i < legend.length; i++) {
+                char direction = legend[i];
+                boolean inBounds = inBounds(direction);
+                if (inBounds) {
+                    char hashChar = codeStr.charAt(i);
+                    boolean open = isHashCharMeansOpen(hashChar);
+                    if (open) {
+                        buffer[nValid++] = direction;
+                    }
+                }
+            }
+            char[] open = Arrays.copyOf(buffer, nValid);
+            return open;
+        }
+
+        private int countOpenAndInBounds(String codeStr) {
+            int numOpen = 0;
+            for (int i = 0; i < codeStr.length(); i++) {
+                char hashChar = codeStr.charAt(i);
+                char direction = legend[i];
+                if (isHashCharMeansOpen(hashChar) && inBounds(direction)) {
+                    numOpen++;
+                }
+            }
+            return numOpen;
+        }
+
+        private int shiftY(char direction) {
+            int offset = 0;
+            if (direction == 'U') {
+                offset = -1;
+            } else if (direction == 'D') {
+                offset  = 1;
+            }
+            return y + offset;
+        }
+
+        private int shiftX(char direction) {
+            int offset = 0;
+            if (direction == 'L') {
+                offset = -1;
+            } else if (direction == 'R') {
+                offset  = 1;
+            }
+            return x + offset;
+        }
+
+        private boolean inBounds(char direction) {
+            int newX = shiftX(direction), newY = shiftY(direction);
+            return Game.this.inBounds(newX, newY);
+        }
+
+        @Override
+        public String toString() {
+            return "State{" +
+                    "x=" + x +
+                    ", y=" + y +
+                    ", trail=" + concatTrail() +
+                    ", level=" + level +
+                    '}';
+        }
+    }
+
+    private boolean inBounds(int x, int y) {
+        return x >= 1 && x <= gridWidth && y >= 1 && y <= gridHeight;
+    }
+
+    private static boolean isHashCharMeansOpen(char ch) {
+        ch = Character.toLowerCase(ch);
+        return ch >= 'b' && ch <= 'f';
+    }
+
+    private static final char[] legend = {'U', 'D', 'L', 'R'};
+
+    public static void main(String[] args) {
+        String passcode = "";
+    }
+}
---
+++ advent17/src/test/java/aoc2016/day17/GameTest.java
@@ -0,0 +1,99 @@
+package aoc2016.day17;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import aoc2016.day17.Game.State;
+import org.junit.experimental.theories.suppliers.TestedOn;
+
+public class GameTest {
+/*
+For example, suppose the passcode is hijkl. Initially, you have taken no steps, and so your path is empty: you simply
+find the MD5 hash of hijkl alone. The first four characters of this hash are ced9, which indicate that up is open (c),
+down is open (e), left is open (d), and right is closed and locked (9). Because you start in the top-left corner, there
+are no "up" or "left" doors to be open, so your only choice is down.
+
+Next, having gone only one step (down, or D), you find the hash of hijklD. This produces f2bc, which indicates that you
+can go back up, left (but that's a wall), or right. Going right means hashing hijklDR to get 5745 - all doors closed and
+locked. However, going up instead is worthwhile: even though it returns you to the room you started in, your path would
+then be DU, opening a different set of doors.
+
+After going DU (and then hashing hijklDU to get 528e), only the right door is open; after going DUR, all doors lock.
+(Fortunately, your actual passcode is not hijkl).
+ */
+
+    @Test
+    public void test1() {
+        Game game = new Game("hijkl");
+        State state = game.root();
+        char[] open = state.findOpen();
+        assertArrayEquals("open 1", "D".toCharArray(), open);
+    }
+
+    @Test
+    public void test2() {
+        Game game = new Game("hijkl");
+        State state = game.root();
+        char[] open;
+        state = state.move('D');
+        open = state.findOpen();
+        assertArrayEquals("open 2", "UR".toCharArray(), open);
+    }
+
+    @Test
+    public void test3() {
+        Game game = new Game("hijkl");
+        State state = game.root();
+        char[] open;
+        state = state.move('D');
+        state = state.move('U');
+        open = state.findOpen();
+        assertArrayEquals("open 3", "R".toCharArray(), open);
+    }
+
+    @Test
+    public void test4() {
+        Game game = new Game("hijkl");
+        State state = game.root();
+        state = state.move('D');
+        state = state.move('U');
+        state = state.move('R');
+        char[] open = state.findOpen();
+        assertArrayEquals("open 4", new char[0], open);
+
+    }
+
+    @Test
+    public void testHash() {
+        // hijklDU to get 528e
+        State state = new Game("hijkl").root().move('D').move('U');
+        String hash = state.computeHash();
+        assertEquals("hash chars", "528e", hash.substring(0, 4));
+    }
+
+    @Test
+    public void play1() {
+        // If your passcode were ihgpwlah, the shortest path would be DDRRRD.
+        testPlay("ihgpwlah", "DDRRRD");
+    }
+
+    private void testPlay(String passcode, String shortestPath) {
+        Game game = new Game(passcode);
+        State finalState = game.play(shortestPath.length() + 1);
+        System.out.format("starting with '%s', final state = %s%n", passcode, finalState);
+        assertNotNull("did not win game", finalState);
+        assertEquals("shortest path", shortestPath, finalState.concatTrail());
+    }
+
+    @Test
+    public void play2() {
+        // With kglvqrro, the shortest path would be DDUDRLRRUDRD.
+        testPlay("kglvqrro", "DDUDRLRRUDRD");
+    }
+
+    @Test
+    public void play3() {
+        // With ulqzkmiv, the shortest would be DRURDRUDDLLDLUURRDULRLDUUDDDRR.
+        testPlay("ulqzkmiv", "DRURDRUDDLLDLUURRDULRLDUUDDDRR");
+    }
+}
