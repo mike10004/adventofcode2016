1803aced00c78cd64b53e4b95e320fe7b018f3b5
Michael Chaberski <mchaberski@novetta.com>
2016-12-20 01:09:02 -0500

    day 20 part two

--- advent20_cfire.c
+++ advent20_cfire.c
@@ -13,7 +13,7 @@ int in_range(const long ip, const long range_min, const long range_max)
     return (ip >= range_min) && (ip <= range_max);
 }
 
-int is_blocked(long ip, long** prange_mins, long** prange_maxs, const int num_ranges)
+int is_blocked(long ip, long* prange_mins[], long* prange_maxs[], const int num_ranges)
 {
     int i;
     long* range_mins = *prange_mins;
@@ -33,6 +33,7 @@ int main(int argc, char* argv[])
     int range_idx = 0;
     long range_min, range_max;
     int ngood;
+    long num_unblocked = 0;
     long ip;
     do {
         ngood = fscanf(stdin, "%ld-%ld", &range_min, &range_max);
@@ -43,11 +44,12 @@ int main(int argc, char* argv[])
             range_idx++;
         }
     } while (ngood == 2);
+
     for (ip = IP_MIN; ip <= IP_MAX; ip++) {
         if (!is_blocked(ip, &range_mins, &range_maxs, range_idx)) {
-            fprintf(stdout, "not blocked: %ld\n", ip);
-            break;
+            num_unblocked++;
         }    
     }
+    fprintf(stdout, "%ld ips are unblocked\n", num_unblocked);
     return 0;
 }
