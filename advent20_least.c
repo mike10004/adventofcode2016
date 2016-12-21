2cdc0c01e066b849bb2cbe0faf23a3860b9620d6
Michael Chaberski <mchaberski@novetta.com>
2016-12-21 00:03:14 -0500

    implement reduction function in c

--- advent20_least.c
+++ advent20_least.c
@@ -2,15 +2,15 @@
 
 #define MAX_LINE_LEN 1024
 #define MAX_NUM_RANGES 1200
+#define NUM_EXCEPTIONS 2
+
 #define IP_MIN 0
 #define IP_MAX 4294967295
 
-#ifndef TRUE
-#define TRUE 1
-#endif
-#ifndef FALSE
-#define FALSE 0
-#endif
+typedef enum Boolean {
+    FALSE = 0,
+    TRUE = 1
+} BOOLEAN;
 
 typedef struct Range {
     long min;
@@ -28,7 +28,7 @@ int in_range(const long ip, RANGE* range)
     return (ip >= range->min) && (ip <= range->max);
 }
 
-int is_blocked(long ip, RANGE* ranges, const int num_ranges)
+BOOLEAN is_blocked(long ip, RANGE* ranges, const int num_ranges)
 {
     int i;
     for (i = 0; i < num_ranges; i++) {
@@ -39,23 +39,110 @@ int is_blocked(long ip, RANGE* ranges, const int num_ranges)
     return FALSE;
 }
 
+BOOLEAN are_disjoint(RANGE* a, RANGE* b) {
+    long alow = a->min, blow = b->min, ahigh = a->max, bhigh = b->max;
+    // return a[1] < (b[0] - 1) or b[1] < (a[0] - 1)
+    return (a->max < (b->min - 1)) || (b->max < (a->min - 1));
+}
+
+long find_min(long* values, const int num_values) {
+    BOOLEAN min_set = FALSE;
+    int i;
+    long min0 = -1;
+    for (i = 0; i < num_values; i++) {
+        if (!min_set || (values[i] < min0)) {
+            min0 = values[i];
+        }
+    }
+    return min0;
+}
+
+long find_max(long* values, const int num_values) {
+    BOOLEAN max_set = FALSE;
+    int i;
+    long max0 = -1;
+    for (i = 0; i < num_values; i++) {
+        if (!max_set || (values[i] > max0)) {
+            max0 = values[i];
+        }
+    }
+    return max0;
+}
+
+// return TRUE if reduced, false if it was disjoint (and output not modified)
+BOOLEAN reduce(RANGE* a, RANGE* b, RANGE* output)
+{
+    long values[] = {a->min, a->max, b->min, b->max};
+    if (!are_disjoint(a, b)) {
+        return FALSE;
+    }
+    set_range(output, find_min(values, 4), find_max(values, 4));
+    return TRUE;
+}
+
+void copy_ranges(RANGE* src, const int num_ranges, RANGE* dst) {
+    int i;
+    for (i = 0; i < num_ranges; i++) {
+        set_range(&(dst[i]), (&(src[i]))->min, (&(src[i]))->max);
+    }
+}
+
+void shift_ranges(RANGE* ranges, const int num_ranges, const int except)
+{
+    int i;
+    RANGE *src, *dst;
+    for (i = except; i < (num_ranges - 1); i++) {
+        src = &ranges[i+1];
+        dst = &ranges[i];
+        set_range(dst, src->min, src->max);
+    }
+}
+
+int reduce_all(RANGE* ranges, const int init_num_ranges, RANGE* outputs)
+{
+    int i, j;
+    int num_ranges = init_num_ranges;
+    BOOLEAN all_disjoint = FALSE, istop;
+    RANGE *a, *b;
+    RANGE reduced;
+    copy_ranges(ranges, num_ranges, outputs);
+    while (!all_disjoint) {
+        all_disjoint = TRUE;
+        istop = FALSE;
+        for (i = 0; (i < num_ranges) && !istop; i++) {
+            a = &ranges[i];
+            for (j = i + 1; j < num_ranges; j++) {
+                b = &ranges[j];
+                if (!are_disjoint(a, b)) {
+                    reduce(a, b, &reduced);
+                    set_range(&(outputs[i]), reduced.min, reduced.max);
+                    shift_ranges(outputs, num_ranges, j);
+                    num_ranges--;
+                    istop = TRUE;
+                    all_disjoint = FALSE;
+                    break;
+                }
+            }
+        }
+    }    
+    return num_ranges;
+}
+
 int main0(const long ip_min, const long ip_max) {
     int ngood;
     long range_min, range_max;
     RANGE ranges[MAX_NUM_RANGES];
-    int range_idx = 0;
+    RANGE reduced_ranges[MAX_NUM_RANGES];
+    int range_idx = 0, num_reduced;
     long ip;
     while ((range_idx < MAX_NUM_RANGES) 
             && (ngood = fscanf(stdin, "%ld-%ld", &range_min, &range_max)) == 2) {
         set_range(&(ranges[range_idx]), range_min, range_max);
         range_idx++;
     }
-    for (ip = ip_min; ip <= ip_max; ip++) {
-        if (!is_blocked(ip, ranges, range_idx)) {
-            fprintf(stdout, "least unblocked: %ld\n", ip);
-            break;
-        }    
-    }
+    num_reduced = reduce_all(ranges, range_idx, reduced_ranges);
+    fprintf(stdout, "reduced %d ranges to %d\n", range_idx, num_reduced);
+    
     return 0;
 }
 
