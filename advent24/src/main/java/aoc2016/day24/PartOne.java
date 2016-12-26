660ebd0e318eeb33eb94185de735bf503e2fbe15
Mike Chaberski <mchaberski@novetta.com>
2016-12-26 17:41:33 -0500

    day 24 part one

---
+++ advent24/src/main/java/aoc2016/day24/PartOne.java
@@ -0,0 +1,23 @@
+package aoc2016.day24;
+
+import aoc2016.day24.Duct.Passage;
+import com.google.common.io.CharSource;
+import com.google.common.io.Resources;
+import com.google.common.math.IntMath;
+import org.jgrapht.GraphPath;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class PartOne {
+
+    public static void main(String[] args) throws IOException {
+        CharSource input = Resources.asCharSource(PartOne.class.getResource("/input.txt"), StandardCharsets.US_ASCII);
+        Blueprint blueprint = Blueprint.parse(input);
+        List<GraphPath<Duct, Passage>> paths = blueprint.findShortestPathToNumberedDucts();
+        int totalLength = paths.stream().map(GraphPath::getLength).reduce(0, IntMath::checkedAdd);
+        System.out.format("length: %d%n", totalLength);
+    }
+}
